cea03991f66f37fd14d2644d7577f36593c9493d
Shreyas R <shrer@microsoft.com>
2019-05-06 13:57:38 +0530

    Update UnitTest1.cs

--- FirstChance/FirstChance/UnitTest1.cs
+++ FirstChance/FirstChance/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FirstChance
@@ -9,37 +9,100 @@ namespace FirstChance
         [TestMethod]
         public void TestMethod1()
         {
-            for (int i = 0; i < 1000000000; i++)
-            {
-                try
-                {
-                    throw new Exception("e");
-                }
-                catch
-                {
-
-                }
-            }
+            loop();
         }
-        
+
         [TestMethod]
         public void TestMethod2()
         {
-            for (int i = 0; i < 1000000000; i++)
-            {
-                try
-                {
-                    throw new Exception("e");
-                }
-                catch
-                {
-
-                }
-            }
+            loop();
         }
-        
+
         [TestMethod]
         public void TestMethod3()
+        {
+            loop();
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            loop();
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            loop();
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            loop();
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            loop();
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            loop();
+        }
+
+                [TestMethod]
+        public void TestMethod11()
+        {
+            loop();
+        }
+
+        [TestMethod]
+        public void TestMethod12()
+        {
+            loop();
+        }
+
+        [TestMethod]
+        public void TestMethod13()
+        {
+            loop();
+        }
+
+        [TestMethod]
+        public void TestMethod14()
+        {
+            loop();
+        }
+
+        [TestMethod]
+        public void TestMethod15()
+        {
+            loop();
+        }
+
+        [TestMethod]
+        public void TestMethod16()
+        {
+            loop();
+        }
+
+        [TestMethod]
+        public void TestMethod17()
+        {
+            loop();
+        }
+
+        [TestMethod]
+        public void TestMethod18()
+        {
+            loop();
+        }
+
+        private void loop()
         {
             for (int i = 0; i < 1000000000; i++)
             {
