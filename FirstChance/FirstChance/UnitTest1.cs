eba201e26b4e4ad8dae69ada559bb3260f1c2524
Shreyas R <shrer@microsoft.com>
2019-05-06 13:53:01 +0530

    Update UnitTest1.cs

--- FirstChance/FirstChance/UnitTest1.cs
+++ FirstChance/FirstChance/UnitTest1.cs
@@ -21,5 +21,37 @@ namespace FirstChance
                 }
             }
         }
+        
+        [TestMethod]
+        public void TestMethod2()
+        {
+            for (int i = 0; i < 1000000000; i++)
+            {
+                try
+                {
+                    throw new Exception("e");
+                }
+                catch
+                {
+
+                }
+            }
+        }
+        
+        [TestMethod]
+        public void TestMethod3()
+        {
+            for (int i = 0; i < 1000000000; i++)
+            {
+                try
+                {
+                    throw new Exception("e");
+                }
+                catch
+                {
+
+                }
+            }
+        }
     }
 }
