98fd3362aecf4ba9064fb45dfaf26108ccadef26
Shreyas R <shrer@microsoft.com>
2019-05-06 13:43:41 +0530

    Removed stuff

---
+++ FirstChance/FirstChance/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FirstChance")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FirstChance")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("70a30260-7ddd-4f14-b574-915a28cb3fad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FirstChance/FirstChance/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FirstChance
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                try
+                {
+                    throw new Exception("e");
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
