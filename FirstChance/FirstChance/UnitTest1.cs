529c53a1a1799eb46a98495cadf1532c45c59d69
Shreyas R <shrer@microsoft.com>
2019-05-15 14:21:42 +0530

    Update UnitTest1.cs

--- FirstChance/FirstChance/UnitTest1.cs
+++ FirstChance/FirstChance/UnitTest1.cs
@@ -14,7 +14,7 @@ namespace FirstChance
 
         private void loop()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 100000; i++)
             {
                 try
                 {
